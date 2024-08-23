cb2d801564630cabb20d1473919ea8a69c8bb6a2
hatakecnk <febryafriansyah6@gmail.com>
2024-08-23 22:52:18 +0700

    first commit

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 var typed = new Typed(".multiple-text", {
 	strings:
-	["Student","Hacker","Programmer","Dreamer","Gamer"],
+	["Student","Programmer","Dreamer","Gamer"],
 	typeSpeed: 100,
 	typeSpeed: 100,
 	backDelay: 1000,
